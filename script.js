49e3614a2b8950bbe09b5fed8d584cc8f98dd8cb
Sachin-Shanmugam <sachinshanmugam19@gmail.com>
2025-11-26 16:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const makeChange = (c) => {
   // your name here
+	let quarters = 0;
+    let dimes = 0;
+    let nickels = 0;
+    let pennies = 0;
+
+    // Calculate the number of quarters
+    quarters = Math.floor(c / 0.25);
+    c -= quarters * 0.25;
+
+    // Calculate the number of dimes
+    dimes = Math.floor(c / 0.10);
+    c -= dimes * 0.10;
+
+    // Calculate the number of nickels
+    nickels = Math.floor(c / 0.05);
+    c -= nickels * 0.05;
+
+    // Calculate the number of pennies
+    pennies = Math.floor(c / 0.01);
+    c -= pennies * 0.01;
+
+    // Return the result as an object
+    return { q: quarters, d: dimes, n: nickels, p: pennies };
 };
 
 // Do not the change the code below
